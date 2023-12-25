6994fa1362a926734a83b2140d64c0aa1ce010f5
Antonio Sard González <antoniosardgonzalez@gmail.com>
2023-12-25 17:56:11 +0100

    Fix bootstrap links

--- Boletin2/ejercicio1/ejercicio1.php
+++ Boletin2/ejercicio1/ejercicio1.php
@@ -1,19 +1,38 @@
-<?php
-/**
- * ENUNCIADO:
- * 1. Crear un formulario simple con campos de nombre y correo
- * electrónico. Al enviar el formulario, mostrar un mensaje de
- * bienvenida usando el método GET.
- */
-if(isset($_GET['nombre']) && isset($_GET['email'])){
-    $nombre = $_GET['nombre'];
-    $correo = $_GET['email'];
-    
-    echo "<div class='container mt-3'>";
-    echo "<div class='alert alert-success' role='alert'>";
-    echo "Bienvenido <strong>$nombre</strong>. Su correo es: <strong>$correo</strong>";
-    echo "</div>";
-    echo "</div>";
-}
+<!DOCTYPE html>
+<html lang="en">
 
-?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenido</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+
+<body>
+
+    <?php
+    /**
+     * ENUNCIADO:
+     * 1. Crear un formulario simple con campos de nombre y correo
+     * electrónico. Al enviar el formulario, mostrar un mensaje de
+     * bienvenida usando el método GET.
+     */
+    if (isset($_GET['nombre']) && isset($_GET['email'])) {
+        $nombre = $_GET['nombre'];
+        $correo = $_GET['email'];
+
+        echo "<div class='container mt-3'>";
+        echo "<div class='alert alert-success' role='alert'>";
+        echo "Bienvenido <strong>$nombre</strong>. Su correo es: <strong>$correo</strong>";
+        echo "</div>";
+        echo "</div>";
+    }
+
+    ?>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+
+</html>
--- Boletin2/ejercicio3/ejercicio3.php
+++ Boletin2/ejercicio3/ejercicio3.php
@@ -1,15 +1,40 @@
-<?php
-/**
- * ENUNCIADO:
- * 3. Desarrollar un formulario que solicite un número. Al enviar el
- * formulario, mostrar la tabla de multiplicar de ese número usando el
- * método GET.
- */
+<!DOCTYPE html>
+<html lang="en">
 
- $numero = $_GET['number'];
- echo "Tabla de multiplicar del $numero:</br>";
- for ($i=1; $i <= 10 ; $i++) { 
-    echo "$numero X $i = " . $numero * $i . "</br>";
- }
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Tabla de multiplicar</title>
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
 
-?>
+<body>
+
+   <?php
+   /**
+    * ENUNCIADO:
+    * 3. Desarrollar un formulario que solicite un número. Al enviar el
+    * formulario, mostrar la tabla de multiplicar de ese número usando el
+    * método GET.
+    */
+
+   if (isset($_GET['number'])) {
+      $numero = $_GET['number'];
+
+      echo "<div class='container mt-3'>";
+      echo "<h5>Tabla de multiplicar del $numero:</h5>";
+      echo "<ul class='list-group'>";
+      for ($i = 1; $i <= 10; $i++) {
+         echo "<li class='list-group-item'>$numero X $i = " . $numero * $i . "</li>";
+      }
+      echo "</ul>";
+      echo "</div>";
+   }
+   ?>
+
+   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+
+</html>
